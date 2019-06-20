2fc864a21bd4e0219e8e08b4da4263fc74fba144
Chris Vo <cvo@onica.com>
2019-06-20 09:27:03 -0700

    Added comments to the main.go script.

--- main.go
+++ main.go
@@ -18,33 +18,40 @@ type Color struct {
 func main() {
 	url := "https://api.noopschallenge.com/hexbot"
 
+	// Create color client
 	colorClient := http.Client{
 		Timeout: time.Second * 2,
 	}
 
+	// Make a GET request to the URL
 	req, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// Actually, DO the request
 	res, getErr := colorClient.Do(req)
 
 	if getErr != nil {
 		log.Fatal(getErr)
 	}
 
+	// Read the body of the request
 	body, readErr := ioutil.ReadAll(res.Body)
 
 	if readErr != nil {
 		log.Fatal(readErr)
 	}
 
+	// Declare the color struct
 	color := Color{}
+
+	// Parses through the json-encoded data and stores it inside &color
 	jsonErr := json.Unmarshal(body, &color)
 	if jsonErr != nil {
 		log.Fatal(jsonErr)
 	}
-
+	// Iterate through the colors
 	for _, b := range color.Colors {
 		fmt.Printf("%s", b.Value)
 	}
